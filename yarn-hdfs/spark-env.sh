a4b6a9f2a5de8e2aa4cc0aaf29875423ea4fb2a2
vmk <vm.kattenberg@gmail.com>
2016-12-11 22:02:20 +0100

    Updated yarn-hdfs config for Spark 2.0.2

--- yarn-hdfs/spark-env.sh
+++ yarn-hdfs/spark-env.sh
@@ -1,4 +1,6 @@
-spark_opts="-Djava.security.krb5.conf=${KRB5_CONFIG} -Djava.security.krb5.realm=CUA.SURFSARA.NL -Djava.security.krb5.kdc=kdc.hathi.surfsara.nl"
+spark_krb_opts="-Djava.security.krb5.conf=${KRB5_CONFIG} -Djava.security.krb5.realm=CUA.SURFSARA.NL -Djava.security.krb5.kdc=kerberos1.osd.surfsara.nl"
 
-export SPARK_SUBMIT_OPTS="${spark_opts}"
-export SPARK_HISTORY_OPTS="${spark_opts}"
+export SPARK_SUBMIT_OPTS="${spark_krb_opts} ${SPARK_SUBMIT_OPTS}"
+export SPARK_HISTORY_OPTS="${spark_krb_opts} ${SPARK_HISTORY_OPTS}"
+
+export SPARK_DIST_CLASSPATH=$(hadoop classpath)
